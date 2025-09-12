dd4fb55db49e228179969bf3ad98d099709b3c26
zkaplan1103 <zkaplan1103@gmail.com>
2025-09-12 11:16:27 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "./App.css";
+import ChatWindow from "./components/ChatWindow";
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="heading">
+        Instalily Case Study
+      </div>
+        <ChatWindow/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+
+export const getAIMessage = async (userQuery) => {
+
+  const message = 
+    {
+      role: "assistant",
+      content: "Connect your backend here...."
+    }
+
+  return message;
+};
